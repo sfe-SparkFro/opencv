6e8a2245ddcf849cef74519a71f00dd9abbfcfeb
Dryw Wade <dryw.wade@sparkfun.com>
2025-05-21 11:19:27 -0600

    Add .cmake files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,35 @@
+# CMake generated Testfile for 
+# Source directory: /home/dryw/micropython-opencv/src/opencv
+# Build directory: /home/dryw/micropython-opencv/src/opencv/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("3rdparty/zlib")
+subdirs("3rdparty/openjpeg")
+subdirs("3rdparty/libpng")
+subdirs("include")
+subdirs("modules/.firstpass/3d")
+subdirs("modules/.firstpass/calib")
+subdirs("modules/.firstpass/core")
+subdirs("modules/.firstpass/dnn")
+subdirs("modules/.firstpass/features")
+subdirs("modules/.firstpass/flann")
+subdirs("modules/.firstpass/highgui")
+subdirs("modules/.firstpass/imgcodecs")
+subdirs("modules/.firstpass/imgproc")
+subdirs("modules/.firstpass/java")
+subdirs("modules/.firstpass/js")
+subdirs("modules/.firstpass/objc")
+subdirs("modules/.firstpass/objdetect")
+subdirs("modules/.firstpass/photo")
+subdirs("modules/.firstpass/python")
+subdirs("modules/.firstpass/stereo")
+subdirs("modules/.firstpass/stitching")
+subdirs("modules/.firstpass/ts")
+subdirs("modules/.firstpass/video")
+subdirs("modules/.firstpass/videoio")
+subdirs("modules/.firstpass/world")
+subdirs("modules/core")
+subdirs("modules/imgproc")
+subdirs("modules/imgcodecs")
+subdirs("doc")
